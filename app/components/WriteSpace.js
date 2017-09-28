2c7f9c64d8cd09d434446d409ec718f0c1d94052
corvids <joonnifer@gmail.com>
2017-09-28 15:18:59 -0400

    fix routing on navbar and update buttons on landing page

--- app/components/WriteSpace.js
+++ app/components/WriteSpace.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import ReactQuill from 'react-quill'
 import Navbar from './Navbar'
+import RaisedButton from 'material-ui/RaisedButton';
 
-class WriteSpace extends Component {
+const style = {
+  margin: 10,
+};
+
+class WriteSpace extends Component{
   constructor(props) {
     super(props)
     this.state = { text: '' } // You can also pass a Quill Delta here
@@ -20,6 +25,10 @@ class WriteSpace extends Component {
         <ReactQuill value={this.state.text}
                     onChange={this.handleChange}
                     className="container container-fluid"/>
+        <RaisedButton label="READ"
+                      backgroundColor="#D2B48C"
+                      style={style}
+                      onClick={(e)=>{this.handleLink(e, "read")}} />
       </div>
     )
   }
