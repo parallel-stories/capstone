1440ba02ff929f64ae05eaca624f47d5f446329d
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-10-02 15:25:30 -0400

    starting downClick nav

--- app/components/StoryBranchNav.jsx
+++ app/components/StoryBranchNav.jsx
@@ -32,12 +32,11 @@ class StoryBranchNav extends Component {
       selector: 0
     }
     this.handleRightClick = this.handleRightClick.bind(this)
+    this.handleLeftClick = this.handleLeftClick.bind(this)
   }
 
   componentDidMount() {
-    console.log(this.props)
     if (_.isEmpty(this.props.currentStoryBranch)) {
-      console.log('EMPTY STORY')
       const storyBranchId = this.props.match.params.branchId
       // getCard.call(this, storyBranchId, this.props.match.params.cardId, this.props.handleCurrentStoryChange)
       firebase.database().ref(`storyBranch/${storyBranchId}`).once('value', snap => {
@@ -46,27 +45,31 @@ class StoryBranchNav extends Component {
       })
       .then(() => {
         firebase.database().ref(`storyCard/${this.props.match.params.cardId}`).once('value', snap => {
-          console.log('set val after empty')
           this.setState({cards: [...this.state.cards, snap.val()]})
         })
       })
     } else {
-      console.log('THE ELSE', this.props.match.params.cardId)
       firebase.database().ref(`storyCard/${this.props.match.params.cardId}`).once('value', snap => {
-        console.log('set val if not empty')
         this.setState({cards: [...this.state.cards, snap.val()]})
       })
     }
-    console.log('HELLO')
+    
   }
 
   handleRightClick = (evt) => {
-    //put logic that is no next card, don't do anything OR disable button
     const nextCardId = this.state.cards[this.state.selector].nextCard
-    console.log('NEXT CARD', nextCardId)
-    firebase.database().ref(`storyCard/${nextCardId}`).once('value', snap => {
-      this.setState({selector: this.state.selector + 1, cards: [...this.state.cards, snap.val()]})
-    })
+
+    if(nextCardId !== ""){
+      firebase.database().ref(`storyCard/${nextCardId}`).once('value', snap => {
+        this.setState({selector: this.state.selector + 1, cards: [...this.state.cards, snap.val()]})
+      })
+    }
+  }
+
+  handleLeftClick = (evt) => {
+    if(this.state.selector){
+      this.setState({selector: this.state.selector - 1})
+    }
   }
 
   render() {
@@ -77,7 +80,7 @@ class StoryBranchNav extends Component {
           <IconButton className="swipe-btn-up-down"><UpArrow/></IconButton>
         </div>
         <div className="row card-container">
-          <IconButton className="col swipe-btn-left-right">
+          <IconButton className="col swipe-btn-left-right" onClick={this.handleLeftClick}>
             <LeftArrow/>
           </IconButton>
           <ReactSwipe className="col carousel"
