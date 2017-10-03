22a278a203093a88bfeaec7f1aa36fd0023ce895
Mieka Page <github@miekapage.com>
2017-10-03 15:38:23 -0400

    save & publish and add associated branches and roots

--- app/components/WriteSpace.js
+++ app/components/WriteSpace.js
@@ -15,26 +15,31 @@ import TextField from 'material-ui/TextField'
 import firebase from 'app/fire'
 import 'firebase/database'
 
-import {dialogStyle} from '../stylesheets/MatUIStyle'
+import { dialogStyle } from '../stylesheets/MatUIStyle'
+
+import { saveCard, publishCard } from './functions/write.js'
 
 export default class WriteSpace extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      userId: 1,
       openSubmit: false,
-      text: '',
-      title: '',
-      dbTitle: '',
-      cardId: '1',
+      dirtyText: false,
+      // saveCard & publishCard depend on the state below not changing
+      cardId: this.props.cardId || '',
+      card: {
+        userId: 1,
+        text: '',
+        branchTitle: 'House of Leaves',
+        rootTitle: this.props.rootTitle || '',
+        prevCard: this.props.prevCard || '',
+        nextCard: this.props.nextCard || ''
+      }
       // TODO: implement warnings
       // to check for inputs -- can't be blank
       // and can't be more than 500 characters
-      dirtyText: false,
-      dirtyTitle: false,
     }
     this.changeStoryText = this.changeStoryText.bind(this)
-    this.changeTitle = this.changeTitle.bind(this)
 
     this.handleOpen = this.handleOpen.bind(this)
     this.handleClose = this.handleClose.bind(this)
@@ -45,17 +50,15 @@ export default class WriteSpace extends Component {
   }
 
   componentDidMount() {
-    if (this.state.cardId) {
-      firebase.database().ref('storyCard').child(this.state.cardId).on('value', snap => {
+    if (this.props.cardId) {
+      firebase.database().ref('storyCard').child(this.props.cardId).on('value', snap => {
         if (!snap.val().published) {
-          this.setState({
-            text: snap.val().text,
-            title: snap.val().branchTitle,
-            dbTitle: snap.val().branchTitle
-          })
+          this.setState({card: snap.val()})
         } else {
           this.setState({
-            text: 'This card has already been published.'
+            card: {
+              text: 'This card has already been published.'
+            }
           })
         }
       })
@@ -64,96 +67,44 @@ export default class WriteSpace extends Component {
 
   changeStoryText(value) {
     this.setState({
-      text: value,
-      dirtyText: true
-    })
-  }
-
-  changeTitle(evt) {
-    this.setState({
-      title: evt.target.value,
-      dirtyTitle: true
+      dirtyText: true,
+      card: Object.assign({}, this.state.card, {text: value})
     })
   }
 
   // clear story and submit story handlers
-  clearStory = () => { this.setState({text: ''}) }
+  clearStory = () => {
+    this.setState({card: {text: ''}})
+  }
 
   saveStory(evt) {
     evt.preventDefault()
 
-    const card = {
-      previousCard: '',
-      nextCard: '',
-      text: this.state.text,
-      branchTitle: this.state.title
-    }
+    const cardKey = saveCard(this.state.card, this.state.cardId) // imported from functions folder. returns card ID
 
-    if (this.state.cardId == null) {
-      const cardKey = firebase.database().ref('storyCard').push(card).key
-
-      const branch = {
-        storyCards: [cardKey],
-        storyRoot: this.state.title
-      }
-
-      const root = {}
-      root[this.state.title] = true
-
-      firebase.database().ref('storyBranch').child(this.state.title).set(branch)
-      firebase.database().ref('storyRoot').child(this.state.title).set(root)
-      firebase.database().ref('user').child(this.state.userId).child('storyBranches').child(this.state.title).set(true)
-      firebase.database().ref('user').child(this.state.userId).child('storyBranches').child('unpublished').child(this.state.title).set(true)
-
-      this.setState({cardId: cardKey})
-    } else {
-      firebase.database().ref('storyCard').child(this.state.cardId).update(card)
-      if (this.state.title !== this.state.dbTitle) {
-        firebase.database().ref('storyBranch').child(this.state.dbTitle).once('value')
-        .then(snap => {
-          const data = snap.val()
-          data['storyRoot'] = this.state.title
-          firebase.database().ref('storyBranch').child(this.state.dbTitle).set(null)
-          return firebase.database().ref('storyBranch').child(this.state.title).set(data)
-        })
-        // firebase.database().ref('storyBranch').child(this.state.title).set(branch)
-        // firebase.database().ref('storyRoot').child(this.state.title).set(root)
-        // firebase.database().ref('user').child(this.state.userId).child('storyBranches').child(this.state.title).set(true)
-        // firebase.database().ref('user').child(this.state.userId).child('storyBranches').child('unpublished').child(this.state.title).set(true)
-        // LOOK INTO BULK UPDATE
-      }
-    }
+    this.setState({
+      cardId: cardKey,
+      dirtyText: false,
+      dirtyTitle: false
+    })
   }
 
   publishStory(evt) {
     evt.preventDefault()
 
-    const card = {
-      previousCard: '',
-      nextCard: '',
-      text: this.state.text,
-      published: true
-    }
-
-    const cardKey = firebase.database().ref('storyCard').push(card).key
-
-    const branch = {
-      storyCards: [cardKey],
-      storyRoot: this.state.title
-    }
-
-    const root = {}
-    root[this.state.title] = true
-
-    firebase.database().ref('storyBranch').child(this.state.title).set(branch)
-    firebase.database().ref('storyRoot').child(this.state.title).set(root)
+    const cardKey = publishCard(this.state.card, this.state.cardId) // imported from functions folder. returns card ID
 
     this.setState({
       openSubmit: false,
-      text: '',
-      title: '',
       dirtyText: false,
       dirtyTitle: false,
+      cardId: '',
+      card: Object.assign({}, this.state.card, {
+        userId: 1,
+        text: '',
+        prevCard: cardKey,
+        nextCard: ''
+      })
     })
   }
 
@@ -165,22 +116,16 @@ export default class WriteSpace extends Component {
     // actions to submit/cancel story submission
     const actionsDialog = [
       <FlatButton key='cancel' label="Cancel" primary={true} onClick={this.handleClose} />,
-      <FlatButton key='submit' label="Publish" primary={true} keyboardFocused={true} onClick={this.publishStory} />,
+      <FlatButton key='submit' label="Publish Card & Continue Story" primary={true} keyboardFocused={true} onClick={this.publishStory} />,
     ]
 
     return (
       <div>
         <div className="row">
           <div className="col-sm-12 col-md-12 col-lg-12">
-            <div className="form-group container">
-              <input type="text"
-                className="form-control"
-                value={this.state.title}
-                placeholder="Story Title"
-                id="titleField"
-                onChange={this.changeTitle} />
-            </div>
-            <ReactQuill value={this.state.text}
+          Root Title: {this.state.card.rootTitle}<br />
+          Branch Title: {this.state.card.branchTitle}<br />
+            <ReactQuill value={this.state.card.text}
               onChange={this.changeStoryText}
               className="container container-fluid" />
           </div>
@@ -189,15 +134,15 @@ export default class WriteSpace extends Component {
           <div className="col-sm-12 col-md-12 col-lg-12">
             <div className="form-group container floatLeft">
               <RaisedButton key='save'
-                label="SAVE"
+                label="SAVE CARD"
                 backgroundColor="#D2B48C"
                 onClick={this.saveStory}
-                disabled={!this.state.text.length} />
+                disabled={!this.state.card.text.length} />
               <RaisedButton key='submit'
-                label="PUBLISH"
+                label="PUBLISH CARD & CONTINUE STORY"
                 backgroundColor="#D2B48C"
                 onClick={this.handleOpen}
-                disabled={!this.state.text.length} />
+                disabled={!this.state.card.text.length} />
               <RaisedButton key='clear'
                 label="CLEAR"
                 backgroundColor="#B83939"
@@ -214,16 +159,13 @@ export default class WriteSpace extends Component {
           contentStyle={dialogStyle}
           autoScrollBodyContent={true}
         >
-          <form onSubmit={this.publishStory}>
-
-          </form>
-          { ReactHtmlParser(this.state.text) }
+          <form onSubmit={this.publishStory} />
+          { ReactHtmlParser(this.state.card.text) }
         </Dialog>
       </div>
     )
   }
 }
-
 
 // <TextField
 // hintText="Name Your Story Line"
@@ -233,3 +175,12 @@ export default class WriteSpace extends Component {
 // multiLine={true}
 // onChange={this.changeTitle}
 // />
+
+// <div className="form-group container">
+// <input type="text"
+//   className="form-control"
+//   value={this.state.title}
+//   placeholder="Story Title"
+//   id="titleField"
+//   onChange={this.changeTitle} />
+// </div>
---
+++ app/components/functions/write.js
@@ -0,0 +1,72 @@
+import firebase from 'app/fire'
+import 'firebase/database'
+
+const createCard = function(card) {
+  // create card & generate random firebase key
+  const cardKey = firebase.database().ref('storyCard').push(card).key
+  // update previous card to point to this one
+  if (card.prevCard) {
+    firebase.database().ref('storyCard').child(card.prevCard).child('nextCard').set(cardKey)
+  }
+  // check if branch exists (by checking storyCards so you can get that return value)
+    // if so, add card to card array
+    // if not, create branch, mark unpublished, create card array
+  firebase.database().ref('storyBranch').child(card.branchTitle).child('storyCards').once('value').then(snap => {
+    if (snap.exists()) {
+      const cards = [...snap.val(), cardKey]
+      firebase.database().ref('storyBranch').child(card.branchTitle).child('storyCards').set(cards)
+    } else {
+      const branch = {}
+      branch.storyRoot = 'unpublished'
+      branch.storyCards = [cardKey]
+      firebase.database().ref('storyBranch').child(card.branchTitle).set(branch)
+    }
+  })
+  // add branch to user branches if it doesn't exist
+  firebase.database().ref('user').child(card.userId).child('storyBranches').child(card.branchTitle).set(true)
+  // WriteSpace needs key back for local state
+  return cardKey
+}
+
+const createOrUpdateCard = function(card, cardId) {
+  let cardKey = cardId
+  if (cardId == '') {
+    cardKey = createCard(card) // returns firebase key
+  } else {
+    firebase.database().ref('storyCard').child(cardId).update(card)
+  }
+  return cardKey // WriteSpace expects key back
+}
+
+export const saveCard = function(card, cardId) {
+  card.published = false
+  return createOrUpdateCard(card, cardId) // returns firebase key
+}
+
+export const publishCard = function(card, cardId) {
+  card.published = true
+  const cardKey = createOrUpdateCard(card, cardId) // returns firebase key
+
+  // if branch's root is marked unpublished, create storyroot with branch's name, include that branch as child of root, and update branch's root from unpublished to new name
+  firebase.database().ref('storyBranch').child(card.branchTitle).child('storyRoot').once('value').then(snap => {
+    if (snap.val() == 'unpublished') {
+      firebase.database().ref('storyBranch').child(card.branchTitle).child('storyRoot').set(card.branchTitle)
+      firebase.database().ref('storyRoot').child(card.branchTitle).child(card.branchTitle).set(true)
+    }
+  })
+  // make sure branch is marked as published so that name cannot be rewritten
+  firebase.database().ref('storyBranch').child(card.branchTitle).child('published').set(true)
+
+  return cardKey // WriteSpace expects key back
+}
+
+//   const branch = {
+//     storyCards: [cardKey],
+//     storyRoot: this.state.title
+//   }
+
+//   const root = {}
+//   root[this.state.title] = true
+
+//   firebase.database().ref('storyBranch').child(this.state.title).set(branch)
+//   firebase.database().ref('storyRoot').child(this.state.title).set(root)
