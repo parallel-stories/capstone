af5e9cce6a493284147857379513fe1dfbdd7dd6
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-10-05 16:25:34 -0400

    feat: sucessfully adding to storyRoot schema when branching

--- app/utils/write.js
+++ app/utils/write.js
@@ -10,24 +10,29 @@ const createCard = function(card) {
   }
   // check if branch exists (by checking storyCards so you can get that return value)
     // if so, add card to card array
-    // if not, create branch, mark unpublished, create card array
   firebase.database().ref('storyBranch').child(card.branchTitle).child('storyCards').once('value').then(snap => {
     if (snap.exists()) {
       const cards = [...snap.val(), cardKey]
       firebase.database().ref('storyBranch').child(card.branchTitle).child('storyCards').set(cards)
-    } else {
+    } 
+    // else if branch does not exist, create branch and create card array
+    else {
       const branch = {}
-      //if there is no root, set storyRoot to unpublished
-      //else if there is a root add it to storyRoot array
+      //if there is no root, set storyRoot to be isRoot
       if (card.rootTitle == '') {
+        //create cardArray
         branch.storyCards = [cardKey]
         // set storybranch's root
         const isRoot = ['isRoot']
-        firebase.database().ref('storyBranch').child(card.branchTitle).child('storyRoot').set(isRoot)
+        branch.storyRoot = isRoot
+        firebase.database().ref('storyBranch').child(card.branchTitle).set(branch)
         // add root to storycard
         firebase.database().ref('storyCard').child(cardKey).child('rootTitle').set(isRoot)
-        firebase.database().ref('storyBranch').child(card.branchTitle).set(branch)
-      } else {
+        //in same level as branches, give key isRoot set to true
+        firebase.database().ref('storyRoot').child(card.branchTitle).child('isRoot').set(true)
+      } 
+      //else if there is a root add it to storyRoot array
+      else {
         firebase.database().ref('storyBranch').child(card.rootTitle).once('value').then(snap => {
           const branchPoint = snap.val().storyCards.indexOf(card.prevCard)
           const rootCards = snap.val().storyCards.slice(0, branchPoint+1)
@@ -35,6 +40,7 @@ const createCard = function(card) {
           branch.storyRoot = [...snap.val().storyRoot, card.rootTitle]
           firebase.database().ref('storyBranch').child(card.branchTitle).set(branch)
           firebase.database().ref('storyCard').child(cardKey).child('rootTitle').set(branch.storyRoot)
+          firebase.database().ref('storyRoot').child(card.rootTitle).child(card.branchTitle).set(true)
         })
       }
     }
@@ -71,24 +77,9 @@ export const publishCard = function(card, cardId) {
   card.published = true
   const cardKey = createOrUpdateCard(card, cardId) // returns firebase key
 
-  // if branch's root is marked unpublished, create storyroot with branch's name, set storyRoot to array with element isRoot as child of root, and update branch's root from unpublished to new name
-  //*******************NEED TO REFACtoR TO MAP THRU CHILDREN */
   // **** BUG: AFTER WE PUBLISH 2nd CARD IN BRANCH IS WHE BRANCH PUBLISHED IS SET TO TRUE
-  // firebase.database().ref('storyRoot').child(card.branchTitle).child('storyRoot').once('value').then(snap => {
-  //   if (snap.val() == 'unpublished') {
-
-  //     // set a root in storyroots
-  //     firebase.database().ref('storyRoot').child(card.branchTitle).set(true)
-      
-  //     // make sure branch is marked as published so that name cannot be rewritten
-  //     firebase.database().ref('storyBranch').child(card.branchTitle).child('published').set(true)
-  //   } else {
-      //logic for if a branch's root is published
-      // make sure branch is marked as published so that name cannot be rewritten
-      firebase.database().ref('storyBranch').child(card.branchTitle).child('published').set(true)
-  //  }
-  // })
-
+  //set branch to published so its name cant be edited
+  firebase.database().ref('storyBranch').child(card.branchTitle).child('published').set(true)
 
   return cardKey // WriteSpace expects key back
 }
