f8a5e8b5342e5b2f0ed5cf55c598a03c6a2d1e1f
corvids <joonnifer@gmail.com>
2017-10-05 16:34:45 -0400

    update user page

--- app/components/UserPage.jsx
+++ app/components/UserPage.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+// react
+import React, { Component } from 'react'
 
-const UserPage = () => (
-  <div>
-    <p>Navigated to a user page</p>
-  </div>
-);
+// firebase
+import firebase from 'app/fire'
+import 'firebase/database'
+const auth = firebase.auth()
 
-export default UserPage;
+//components
+import AllStoryBranches from './AllStoryBranches'
+
+export default class UserPage extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      user: {},
+      storyBranches: {},
+      favorites: {},
+    }
+
+  }
+
+  render() {
+    const { user, storyBranches, favorites } = this.state || {}
+
+    return (
+      <div className="container-fluid" >
+        <div>
+          <h1><em>User: </em> {user.displayName || 'This user has no name'}</h1>
+          <p> A description will go here </p>
+          <hr />
+          <h2>Stories Authored</h2>
+          { !storyBranches || !Object.keys(storyBranches).length?
+          <div>
+            <p>
+              It looks like this user didn't write any stories yet. 😢
+            </p>
+          </div>
+          :
+          <div className="row" >
+            <AllStoryBranches searchResults={storyBranches} searching={true} />
+          </div>
+          }
+          <h2>This user's favorite stories</h2>
+          { !favorites || !Object.keys(favorites).length ?
+          <div>
+            <p>
+              It looks like this user didn't like any stories yet. 😢
+            </p>
+          </div>
+          :
+          <div className="row" >
+            <AllStoryBranches searchResults={storyBranches} searching={true} />
+          </div>
+          }
+        </div>
+      </div>
+    )
+  }
+  }
