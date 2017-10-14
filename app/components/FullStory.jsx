883673adc743c0da4ba972317b9c2d6770c4fa4f
corvids <joonnifer@gmail.com>
2017-10-14 19:10:13 -0400

    PDF does not print out objects only strings

--- app/components/FullStory.jsx
+++ app/components/FullStory.jsx
@@ -125,11 +125,11 @@ export default class SingleStoryPage extends Component {
 
       for( let i=0; i<cardText.length; i++ ) {
         let currCard = ReactHtmlParser(this.state.storyCards[idx].text)[i].props.children
-        fullText += currCard.toString() + '\n' + '\n'
+
+        if( typeof currCard[0] === 'string')
+          fullText += currCard.toString() + '\n' + '\n'
       }
     }
-
-    console.log('full text....', fullText)
 
     for (var i in fonts) {
       if (fonts.hasOwnProperty(i)) {
